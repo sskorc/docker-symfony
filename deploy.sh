947def59d49e38770e55d50c53165c200b2319c4
Szymon Skórczyński <szymon.skorczynski@schibsted.pl>
2016-02-06 19:46:08 +0100

    Further deploy config changes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
+docker tag sskorc/docker-symfony-dist:latest sskorc/docker-symfony-dist:$TRAVIS_BUILD_NUMBER
+
 docker push sskorc/docker-symfony-dist
+
+curl -u sskorc:$TUTUM_API_KEY -H "Content-Type: application/json" -X POST -d '{"reuse_volumes":false}' https://dashboard.tutum.co/api/v1/service/$PHP_SERVICE_UUID/redeploy/
